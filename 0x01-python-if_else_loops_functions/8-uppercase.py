85daf204b0d8e247eb64f8c9aa74cde1358f11ff
allanyego <allanyego05@gmail.com>
2024-03-19 21:07:11 +0300

    Fix failing script

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,6 @@ def uppercase(str):
     out = ""
     for l in str:
         co = ord(l)
-        print(l, co, sep=' - ')
         # convert lowercase
         if co >= 97 and co <= 122:
             out += chr(co - 32)  # A is 32 less aout += l
