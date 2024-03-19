a9ecfa6712a775c7460986ca8b02a4d75572b8d7
allanyego <allanyego05@gmail.com>
2024-03-19 18:39:28 +0300

    Fix buggy if...elif

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ last_digit = abs(number) % 10  # abs ensures unsigned
 print(f"Last digit of {number} is {last_digit} and ", end='')
 if last_digit == 0:
     print("is 0")
-elif last_digit > 5:
-    print("is greater than 5")
 elif last_digit < 6 and last_digit != 0:
     print("is less than 6 and not 0")
+elif last_digit > 5:
+    print("is greater than 5")
