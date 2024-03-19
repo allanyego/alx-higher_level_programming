611b035e977dea5b09a18c87ca6e4551014ecd37
allanyego <allanyego05@gmail.com>
2024-03-19 18:43:44 +0300

    Fix buggy if...elif

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last_digit = abs(number) % 10  # abs ensures unsigned
+last_digit = abs(number) % 10  # abs ensures unsigned modulus
+if number < 0:
+    last_digit = -(last_digit)  # reset sign
 print(f"Last digit of {number} is {last_digit} and ", end='')
 if last_digit == 0:
     print("is 0")
