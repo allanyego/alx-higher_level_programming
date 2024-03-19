79cf705ae1701e95902a0775cd774e0ddc3a9d8b
allanyego <allanyego05@gmail.com>
2024-03-20 00:14:26 +0300

    Fix tuple addition script

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -6,11 +6,22 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if len(tuple_a) > len(tuple_b):
         cntrl, othr = tuple_a, tuple_b
 
-    curr = 0
-    while curr < 2:
+    for i, num_a in enumerate(cntrl):
+        if i > 1:
+            break
+        
         num_b = 0
         if i < len(othr):
             num_b = othr[i] 
         out.append(num_a + num_b)
         
     return tuple(out)
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
+print(add_tuple((3, 21), (8,)))
+print(add_tuple((11, 8), (12, 12, 9)))
