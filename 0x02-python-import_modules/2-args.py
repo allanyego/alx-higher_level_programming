93e5818e78c6501ca8a5c32d9916cfaa950f7737
allanyego <allanyego05@gmail.com>
2024-03-20 12:47:05 +0300

    Fix buggy scripts

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 import sys
 if __name__ == "__main__":
     arg_len = len(sys.argv) - 1
-    print("{} argument{}:".format(arg_len, "s" if arg_len > 1 else ""))
+    print("{} argument{}".format(arg_len, "" if arg_len == 1 else "s"), end='')
+    print("{}".format("." if arg_len == 0 else ":"))
     for i, arg in enumerate(sys.argv):
         if i == 0:
             continue
