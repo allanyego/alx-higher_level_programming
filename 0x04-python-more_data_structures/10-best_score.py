91af7d3664f22a5788a2d2f03b2011d73ba42ee6
allanyego <allanyego05@gmail.com>
2024-03-21 12:20:11 +0300

    Fix buggy scripts

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     lgst = None
-    for k, v in a_dictionary:
-        if lgst is None and v > lgst:
-            lgst = v
+    if a_dictionary is not None:
+        for k, v in a_dictionary:
+            if lgst is None and v > lgst:
+                lgst = v
+    return lgst
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     out = {}
-    for k, v in a_dictionary:
+    for k, v in a_dictionary.items():
         out[k] = v * 2
     return out
