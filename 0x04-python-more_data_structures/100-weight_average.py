446238946051b23be78e075d20466dc997a70136
allanyego <allanyego05@gmail.com>
2024-03-21 12:42:40 +0300

    Implement advanced tasks

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    averaged = 0
+    total = 0
+
+    for a, b in my_list:
+        average += a * b
+        total += b
+
+    return averaged / total
