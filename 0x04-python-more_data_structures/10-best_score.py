49a3e04cdd9ff2919c46ca361cdc91140c8fa8fb
allanyego <allanyego05@gmail.com>
2024-03-21 12:23:03 +0300

    Fix buggy script

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,6 @@ def best_score(a_dictionary):
     lgst = None
     if a_dictionary is not None:
         for k, v in a_dictionary:
-            if lgst is None and v > lgst:
-                lgst = v
+            if lgst is None or v > lgst:
+                lgst = k
     return lgst
